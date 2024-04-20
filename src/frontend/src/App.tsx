c483fe89b5869e80c322bbc4d77d7b097e66fcd5
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-04-20 12:37:57 +0200

    change frontend to a basic hello world

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,8 +1,9 @@
 function App() {
-  return <>
-    <h1>Hello, world!</h1>
-    <p>Welcome to your Electron app.</p>
-  </>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
