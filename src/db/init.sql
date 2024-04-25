f8c82d21298a30ee71d00792ee3fa697335a9a35
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-04-25 15:19:05 +0200

    Separate the databases into different containers

--- src/db/init.sql
+++ src/db/init.sql
@@ -1,9 +1,10 @@
 -- Active: 1713614538968@@127.0.0.1@5432@app
 -- CREATE DATABASE "app";
-CREATE TYPE EVENT_STATUS AS ENUM ('YES', 'NO', 'MAYBE', 'PENDING');
+CREATE TYPE INVITE_STATUS AS ENUM ('YES', 'NO', 'MAYBE', 'PENDING');
+CREATE TYPE PARTICIPATION_STATUS AS ENUM ('YES', 'NO', 'MAYBE');
 CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
-    "username" TEXT NOT NULL,
+    "username" TEXT UNIQUE NOT NULL,
     "password" TEXT NOT NULL
 );
 CREATE TABLE "events" (
@@ -11,25 +12,30 @@ CREATE TABLE "events" (
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "date" DATE NOT NULL,
-    "organizer" TEXT NOT NULL,
-    "public" BOOLEAN NOT NULL DEFAULT TRUE
+    "organizerId" INTEGER NOT NULL,
+    "public" BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY ("organizerId") REFERENCES "users" ("id") ON DELETE CASCADE
 );
-CREATE TABLE "invitations" (
-    "event_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
-    "status" EVENT_STATUS NOT NULL,
-    FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
+CREATE TABLE "invites" (
+    "eventId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "status" INVITE_STATUS NOT NULL DEFAULT 'PENDING',
+    PRIMARY KEY ("eventId", "userId"),
+    FOREIGN KEY ("eventId") REFERENCES "events" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE
 );
-CREATE TABLE "calendars" (
-    "user_id" INTEGER NOT NULL,
-    "event_id" INTEGER NOT NULL,
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE
+CREATE TABLE "participations" (
+    "eventId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "status" PARTICIPATION_STATUS NOT NULL DEFAULT 'MAYBE',
+    PRIMARY KEY ("eventId", "userId"),
+    FOREIGN KEY ("eventId") REFERENCES "events" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE CASCADE
 );
 CREATE TABLE "shared_calendars" (
-    "owner_id" INTEGER NOT NULL,
-    "user_id" INTEGER NOT NULL,
-    FOREIGN KEY ("owner_id") REFERENCES "users" ("id") ON DELETE CASCADE,
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE
+    "sharing_user_id" INTEGER NOT NULL,
+    "receiving_user_id" INTEGER NOT NULL,
+    FOREIGN KEY ("sharing_user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
+    FOREIGN KEY ("receiving_user_id") REFERENCES "users" ("id") ON DELETE CASCADE,
+    CONSTRAINT "unique_shared_calendar" UNIQUE ("sharing_user_id", "receiving_user_id")
 );
