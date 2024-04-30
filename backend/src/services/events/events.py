497ac24e445fad5b50583951daf4d58f8439c3e9
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-04-30 09:10:11 +0200

    Basic implementation of events service

--- backend/src/services/events/events.py
+++ backend/src/services/events/events.py
@@ -1,10 +1,96 @@
-from fastapi import APIRouter
-
+import json
+from pydantic import BaseModel
+from fastapi import APIRouter, Response, status
+from wrapper import (
+    create_event,
+    delete_event,
+    find_all_events,
+    find_event,
+    update_event,
+)
+import httpx
 router = APIRouter()
+
+class EventModel(BaseModel):
+    title: str
+    description: str
+    date: str
+    organizerId: int
+    isPublic: bool
+
 
 @router.get("/")
 async def get_events():
     """
     Get events.
     """
-    return {"events": []}
+    try:
+        return Response(
+            status_code=status.HTTP_200_OK,
+            content=[json.dumps({"events": [event for event in find_all_events()]})],
+            media_type="application/json",
+        )
+    except Exception as e:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(e)}),
+            media_type="application/json",
+        )
+
+@router.get("/{event_id}")
+async def get_event(event_id: int):
+    """
+    Get an event by its id.
+    """
+    try:
+        event = find_event(event_id)
+        if not event:
+            return Response(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=json.dumps({"error": "Event not found"}),
+                media_type="application/json",
+            )
+        return Response(
+            status_code=status.HTTP_200_OK,
+            content=json.dumps({"event": event}),
+            media_type="application/json",
+        )
+    except Exception as e:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(e)}),
+            media_type="application/json",
+        )
+    
+@router.post("/")
+async def add_event(event: EventModel):
+    """
+    Create an event.
+    """
+    # Check if the organizerId is valid
+    try:
+        response = httpx.get(f"http://auth-service:8000/api/users/{event.organizerId}")
+        if response.status_code != 200:
+            return Response(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=json.dumps({"error": "Organizer not found"}),
+                media_type="application/json",
+            )
+
+        return Response(
+            status_code=status.HTTP_201_CREATED,
+            content=json.dumps({"event": create_event(
+                title=event.title,
+                description=event.description,
+                date=event.date,
+                organizerId=event.organizerId,
+                isPublic=event.isPublic,
+            )}),
+            media_type="application/json",
+        )
+    except Exception as e:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(e)}),
+            media_type="application/json",
+        )
---
+++ backend/src/services/events/wrapper.py
@@ -0,0 +1,224 @@
+import os
+from typing import Any
+
+from sqlalchemy import (
+    Boolean,
+    Column,
+    create_engine,
+    ForeignKey,
+    SmallInteger,
+    String,
+    URL,
+)
+
+from sqlalchemy.exc import IntegrityError, OperationalError
+from sqlalchemy.orm import DeclarativeBase, relationship, Session, sessionmaker
+
+
+def get_env(var: str) -> str:
+    """
+    Return value of an environment variable, raise exception if not defined or empty.
+
+    :param var: The name of the environment variable to retrieve.
+
+    :returns: Value of the environment variable `var`.
+    :raises RuntimeError: If the environment variable `var` is not defined or empty.
+    """
+    if value := os.getenv(var, ""):
+        return value
+    raise RuntimeError(f"{var} is not defined or empty")
+
+
+def get_db_url() -> URL:
+    """
+    Build and return the database URL to connect with postgres.
+
+    :returns: An SQLAlchemy URL object to connect with the appdb.
+    :raises RuntimeError: In case of missing or incorrectly configured env vars.
+    """
+    # Read data from environment
+
+    user = get_env("EVENTS_DB_USER")
+    password = get_env("EVENTS_DB_PASSWORD")
+    host = get_env("EVENTS_DB_HOST")
+    port_raw = get_env("EVENTS_DB_PORT")
+    db = get_env("EVENTS_DB_NAME")
+
+    # Convert port to number
+
+    try:
+        port = int(port_raw)
+    except ValueError as exc:
+        raise RuntimeError(f"Invalid EVENTS_DB_PORT: {port_raw}") from exc
+    return URL.create(
+        drivername="postgresql+psycopg2",
+        username=user,
+        password=password,
+        host=host,
+        port=port,
+        database=db,
+    )
+
+
+def get_session() -> Session:
+    try:
+        session_maker = sessionmaker(bind=create_engine(get_db_url()))
+        session = session_maker()
+    except RuntimeError as exc:
+        # Temporary ugly fix to not crash testing
+
+        session = Session()
+    return session
+
+
+class Base(DeclarativeBase):
+    """
+    Base class for model class
+    """
+
+
+class Event(Base):
+    """
+    Event model class
+    """
+
+    id = Column(SmallInteger, primary_key=True, autoincrement=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    date = Column(String, nullable=False)
+    organizerId = Column(SmallInteger, nullable=False)
+    isPublic = Column(Boolean, nullable=False)
+
+
+def create_event(
+    title: str, description: str, date: str, organizerId: int, isPublic: bool
+) -> Any:
+    """
+    Create an event with the given parameters.
+
+    :param title: The title of the event.
+    :param description: The description of the event.
+    :param date: The date of the event.
+    :param organizerId: The id of the organizer.
+    :param isPublic: Whether the event is public or not.
+
+    :returns: The created event.
+    :raises IntegrityError: If the event could not be created.
+    """
+    event = Event(
+        title=title,
+        description=description,
+        date=date,
+        organizerId=organizerId,
+        isPublic=isPublic,
+    )
+    session = get_session()
+    session.add(event)
+    try:
+        session.commit()
+    except IntegrityError as exc:
+        session.rollback()
+        raise exc
+    return event
+
+
+def find_all_events() -> Any:
+    """
+    Get all events.
+
+    :returns: A list of all events.
+    """
+    session = get_session()
+    events = session.query(Event).all()
+    return [
+        Event(
+            id=event.id,
+            title=event.title,
+            description=event.description,
+            date=event.date,
+            organizerId=event.organizerId,
+            isPublic=event.isPublic,
+        )
+        for event in events
+    ]
+
+
+def find_event(event_id: int) -> Any:
+    """
+    Get an event by its id.
+
+    :param event_id: The id of the event to retrieve.
+
+    :returns: The event with the given id.
+    """
+    session = get_session()
+    event = session.query(Event).filter(Event.id == event_id).first()
+    return (
+        Event(
+            id=event.id,
+            title=event.title,
+            description=event.description,
+            date=event.date,
+            organizerId=event.organizerId,
+            isPublic=event.isPublic,
+        )
+        if event
+        else None
+    )
+
+
+def update_event(
+    event_id: int,
+    title: str,
+    description: str,
+    date: str,
+    organizerId: int,
+    isPublic: bool,
+):
+    """
+    Update an event with the given parameters.
+
+    :param event_id: The id of the event to update.
+    :param title: The title of the event.
+    :param description: The description of the event.
+    :param date: The date of the event.
+    :param organizerId: The id of the organizer.
+    :param isPublic: Whether the event is public or not.
+
+    :returns: The updated event.
+    :raises IntegrityError: If the event could not be updated.
+    """
+    session = get_session()
+    event = session.query(Event).filter(Event.id == event_id).first()
+    if not event:
+        return
+    setattr(event, 'title', title)
+    setattr(event, 'description', description)
+    setattr(event, 'date', date)
+    setattr(event, 'organizerId', organizerId)
+    setattr(event, 'isPublic', isPublic)
+    try:
+        session.commit()
+    except Exception as exc:
+        session.rollback()
+        raise exc
+    return
+
+
+def delete_event(event_id: int):
+    """
+    Delete an event by its id.
+
+    :param event_id: The id of the event to delete.
+    """
+    session = get_session()
+    event = session.query(Event).filter(Event.id == event_id).first()
+    if not event:
+        return
+    session.delete(event)
+    try:
+        session.commit()
+        return
+    except Exception as exc:
+        session.rollback()
+        raise exc
