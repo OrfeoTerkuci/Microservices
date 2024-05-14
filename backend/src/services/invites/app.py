185c4d7acf7bdf9ea1c42be75eb5115821288988
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-05-14 10:40:48 +0200

    Create beginning invites container

---
+++ backend/src/services/invites/app.py
@@ -0,0 +1,27 @@
+"""
+This file contains the event routes for the FastAPI application.
+"""
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import invites
+
+app = FastAPI(
+    title="Invites Service API",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url=None,
+    openapi_url="/openapi.json",
+    root_path="/api",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+app.include_router(invites.router, prefix="/invites", tags=["invites"])
