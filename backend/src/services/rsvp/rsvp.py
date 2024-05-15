7fff2f8b8ee9696a676bc3afd6ed43e3b4026db7
Orfeo Terkuci <terkuciorfeo@gmail.com>
2024-05-15 14:26:34 +0200

    feat: Add RsvpResponse model and CRUD operations for rsvp service

--- backend/src/services/rsvp/rsvp.py
+++ backend/src/services/rsvp/rsvp.py
@@ -5,7 +5,226 @@ import logging
 import httpx
 from fastapi import APIRouter, Query, Response, status
 from pydantic import BaseModel, Field
+from wrapper import (
+    create_response,
+    delete_response,
+    find_all_responses,
+    find_response,
+    find_response_by_event,
+    find_responses_by_user,
+    RSVP_STATUS,
+    update_response,
+)
 
 router = APIRouter()
 
 
+class RsvpResponseModel(BaseModel):
+    eventId: int = Field(..., description="Event's ID")
+    username: str = Field(..., min_length=1, description="User's username")
+    status: RSVP_STATUS = Field(..., description="Response status")
+
+
+def check_user_exists(username: str):
+    response = httpx.get(f"http://auth-service:8000/api/users?username={username}")
+    return response.status_code == 200
+
+
+def check_event_exists(eventId: int):
+    response = httpx.get(f"http://events-service:8000/api/events/{eventId}")
+    return response.status_code == 200 and response.json()["event"]["isPublic"]
+
+
+@router.get("")
+def get_responses():
+    """
+    Get all responses
+    """
+    responses = find_all_responses()
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps(
+            {
+                "responses": [
+                    {
+                        "eventId": response.eventId,
+                        "username": response.username,
+                        "status": response.status,
+                    }
+                    for response in responses
+                ]
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.get("")
+def get_response_by_user(
+    username: str = Query(default=None, description="User's username")
+):
+    """
+    Get response by user
+    """
+    responses = find_responses_by_user(username)
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps(
+            {
+                "responses": [
+                    {
+                        "eventId": response.eventId,
+                        "username": response.username,
+                        "status": response.status,
+                    }
+                    for response in responses
+                ]
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.get("")
+def get_response_by_event(eventId: int = Query(default=None, description="Event's ID")):
+    """
+    Get response by event
+    """
+    responses = find_response_by_event(eventId)
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps(
+            {
+                "responses": [
+                    {
+                        "eventId": response.eventId,
+                        "username": response.username,
+                        "status": response.status,
+                    }
+                    for response in responses
+                ]
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.get("")
+def get_response(
+    username: str = Query(default=None, description="User's username"),
+    eventId: int = Query(default=None, description="Event's ID"),
+):
+    """
+    Get response by user and event ID
+    """
+    response = find_response(eventId, username)
+    if not response:
+        return Response(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content=json.dumps({"error": "Response not found"}),
+            media_type="application/json",
+        )
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps(
+            {
+                "response": {
+                    "eventId": response.eventId,
+                    "username": response.username,
+                    "status": response.status,
+                }
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.post("")
+def create_rsvp(response: RsvpResponseModel):
+    """
+    Create a new response
+    """
+    try:
+        # Check if the user and event exist and are public
+
+        if not check_user_exists(response.username):
+            return Response(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=json.dumps({"error": "User not found"}),
+                media_type="application/json",
+            )
+        if not check_event_exists(response.eventId):
+            return Response(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content=json.dumps({"error": "Public event not found"}),
+                media_type="application/json",
+            )
+        create_response(response.eventId, response.username, response.status)
+    except Exception as exc:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(exc)}),
+        )
+    return Response(
+        status_code=status.HTTP_201_CREATED,
+        content=json.dumps(
+            {
+                "response": {
+                    "eventId": response.eventId,
+                    "username": response.username,
+                    "status": response.status.value,
+                }
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.put("")
+def update_rsvp(response: RsvpResponseModel):
+    """
+    Update a response
+    """
+    try:
+        update_response(response.eventId, response.username, response.status)
+    except Exception as exc:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(exc)}),
+            media_type="application/json",
+        )
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps(
+            {
+                "response": {
+                    "eventId": response.eventId,
+                    "username": response.username,
+                    "status": response.status.value,
+                }
+            }
+        ),
+        media_type="application/json",
+    )
+
+
+@router.delete("")
+def delete_rsvp(response: RsvpResponseModel):
+    """
+    Delete a response
+    """
+    # TODO: Maybe change thee endpoint to /rsvp/{eventId}/{username}
+
+    try:
+        delete_response(response.eventId, response.username)
+    except Exception as exc:
+        return Response(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=json.dumps({"error": str(exc)}),
+            media_type="application/json",
+        )
+    return Response(
+        status_code=status.HTTP_200_OK,
+        content=json.dumps({"message": "Response deleted"}),
+        media_type="application/json",
+    )
--- backend/src/services/rsvp/wrapper.py
+++ backend/src/services/rsvp/wrapper.py
@@ -76,7 +76,124 @@ def get_session() -> Session:
     return session
 
 
+class RSVP_STATUS(enum.Enum):
+    """
+    RSVP status enum
+    """
+
+    YES = "YES"
+    NO = "NO"
+    MAYBE = "MAYBE"
+
+
 class Base(DeclarativeBase):
     """
     Base class for model class
     """
+
+
+class RsvpResponse(Base):
+    """
+    RSVP Responses model
+    """
+
+    __tablename__ = "rsvp_responses"
+
+    eventId = Column(SmallInteger, primary_key=True)
+    username = Column(String, primary_key=True)
+    status = Column(String, default=RSVP_STATUS.MAYBE, nullable=False)
+    # __table_args__ = PrimaryKeyConstraint("eventId", "username")
+
+
+def create_response(eventId: int, username: str, status: RSVP_STATUS):
+    response = RsvpResponse(eventId=eventId, username=username, status=status.value)
+
+    session = get_session()
+    session.add(response)
+    try:
+        session.commit()
+    except (IntegrityError, OperationalError) as exc:
+        session.rollback()
+        raise exc
+    return RsvpResponse(eventId=eventId, username=username, status=status)
+
+
+def find_all_responses():
+    session = get_session()
+    responses = session.query(RsvpResponse).all()
+    return [
+        RsvpResponse(
+            eventId=response.eventId, username=response.username, status=response.status
+        )
+        for response in responses
+    ]
+
+
+def find_response(eventId: int, username: str):
+    if not (eventId or username):
+        return None
+    session = get_session()
+    response = (
+        session.query(RsvpResponse)
+        .filter(RsvpResponse.eventId == eventId, RsvpResponse.username == username)
+        .first()
+    )
+    return (
+        RsvpResponse(
+            eventId=response.eventId, username=response.username, status=response.status
+        )
+        if response
+        else None
+    )
+
+
+def find_response_by_event(eventId: int):
+    session = get_session()
+    responses = (
+        session.query(RsvpResponse).filter(RsvpResponse.eventId == eventId).all()
+    )
+    return [
+        RsvpResponse(
+            eventId=response.eventId, username=response.username, status=response.status
+        )
+        for response in responses
+    ]
+
+
+def find_responses_by_user(username: str):
+    session = get_session()
+    responses = (
+        session.query(RsvpResponse).filter(RsvpResponse.username == username).all()
+    )
+    return [
+        RsvpResponse(
+            eventId=response.eventId, username=response.username, status=response.status
+        )
+        for response in responses
+    ]
+
+
+def update_response(eventId: int, username: str, status: RSVP_STATUS):
+    session = get_session()
+    response = session.query(RsvpResponse).get((eventId, username))
+    if response:
+        setattr(response, "status", status.value)
+        try:
+            session.commit()
+        except Exception as exc:
+            session.rollback()
+            raise exc
+    return
+
+
+def delete_response(eventId: int, username: str):
+    session = get_session()
+    response = session.query(RsvpResponse).get((eventId, username))
+    if response:
+        session.delete(response)
+        try:
+            session.commit()
+        except Exception as exc:
+            session.rollback()
+            raise exc
+    return
